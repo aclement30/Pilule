e03af9e03464930996d678026aeae769cbd72214
Alexandre Clément <a.clement30@gmail.com>
2012-09-05 16:03:58 -0400

    V2

--- application/views/welcome/empty.php
+++
@@ -1,6 +0,0 @@
-<h2 class="title">Tableau de bord<?php if ($_SESSION['cap_datacheck'] == 2) { } else { ?><a id="edit-dashboard-link" href="javascript:dashboardObj.edit();" class="link" style="margin-right: 15px; font-size: 9pt;"><img src="<?php echo site_url(); ?>images/pencil.png" align="absmiddle" />&nbsp;&nbsp;Modifier</a><div class="clear"></div><?php } ?></h2>	
-<div class="clear"></div>
-<div class="page-separator"></div>
-
-<div class="post-content">
-<div class="clear"></div></div>
