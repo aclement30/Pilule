1677a96ea0a5fec65f43257865242d613c7314fb
Alexandre Clément <web@alexandreclement.com>
2012-11-20 22:53:03 -0500

    Update Cache controller (WIP)

--- app/Controller/CacheController.php
+++ app/Controller/CacheController.php
@@ -3,6 +3,10 @@ class CacheController extends AppController {
 	public $name = 'Cache';
 
 	public function reloadData() {
-		
+		return new CakeResponse( array(
+        	'body' => json_encode( array(
+        		
+        	) )
+        ) );
 	}
 }
